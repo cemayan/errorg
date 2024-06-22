45c5d51225938f24f3ece51d285e9f7e44898eba
cayan <cem_ayan@outlook.com>
2024-06-22 23:04:32 +0300

    refactor: comment lines of group.go

--- group.go
+++ group.go
@@ -16,27 +16,24 @@ type Group interface {
 func groupsInitializer() {
 
 	for _, plg := range getConfig().Plugins {
-		// load module
-		// 1. open the so file to load the symbols
 
 		dirname, err := getModuleFolder()
-
 		path := fmt.Sprintf("%s/%s", dirname, plg.Location)
+
+		// Open opens a Go plugin.
 		plug, err := plugin.Open(path)
 		if err != nil {
 			fmt.Errorf("an error occurred while opening plugin: %v", err)
 			return
 		}
 
-		//
+		//Lookup searches for a symbol named symName in plugin p.
 		sym, err := plug.Lookup(symName)
 		if err != nil {
 			fmt.Errorf("an error occurred while looking plugin: %v", err)
 			return
 		}
 
-		// 3. Assert that loaded symbol is of a desired type
-		// in this case interface type Greeter (defined above)
 		var group Group
 		group, ok := sym.(Group)
 		if !ok {
